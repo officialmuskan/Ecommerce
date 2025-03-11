cfe39ef72808b1eee85c7ca04a17f6f60c46b8f0
Muskan <muskanchhabra.0806@gmail.com>
2025-03-11 23:40:55 +0530

    Update app.js
    basic is this

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 //skip at 34
 const express = require("express")
 const app = express();
-app.use(express.json())
-const products require("./routes/productRoute")
+app.use(express.json());
+const products = require("./routes/productRoute")
 app.use('/api/v1', products);
 module.exports=app
