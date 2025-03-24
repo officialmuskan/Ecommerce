f7bbe0e0b3ec8b2bd4a1b6c17132040aea97c384
Muskan <muskanchhabra.0806@gmail.com>
2025-03-24 22:13:49 +0530

    done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,13 +22,41 @@ exports.getAllProducts =  async(req, res)=>{
         products})
 }
 exports.updateProduct = async(req, res, next)=>{
+    try {
     let product = await Product.findById(req.params.id);
     if(!product){
-        res.status(400).json({
+        return res.status(400).json({
             message:"product doesnt exist"
         })
     }
-    product = await Product.findbyIdAndUpdate(req.params.id, req.body)
-    res.status(200).json({product})
+    
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true, 
+            runValidators: true 
+        })
+        res.status(200).json({product})
+    } catch (error) {
+        res.status(200).json({message: error.message})
+    }
+    
+
+}
+
+exports.deleteProduct = async(req, res, next)=>{
+    try {
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(400).json({
+            message:"product doesnt exist"
+        })
+    }
+    
+        await Product.findByIdAndDelete(req.params.id)
+            
+        res.status(200).json({message:"product deleted"})
+    } catch (error) {
+        res.status(200).json({message: error.message})
+    }
+    
 
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct,updateProduct, deleteProduct } = require("../controllers/productController");
 const router = express.Router();
 router.route("/products").get(getAllProducts)
 router.route("/product/new").post(createProduct)
+router.route("/product/update/:id").put(updateProduct)
+router.route("/product/delete/:id").delete(deleteProduct)
 module.exports = router
